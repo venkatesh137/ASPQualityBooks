0ae0d6e82082bab032f6269e4f05f7ec7031a35a
Harjinder Mann <mannharjinder87@gmail.com>
2019-09-01 00:20:21 +1200

    Home Page footer added

---
+++ QualityBooks/QualityBooks/wwwroot/js/imagePreview.js
@@ -0,0 +1,30 @@
+﻿// JS used to display image preview for creating and editing bag items.
+
+var fileInput = document.querySelector('#uploadFile');
+var previewFrame = document.querySelector('#img-preview');
+
+fileInput.addEventListener('change', function () {
+
+    //Remove current image 
+    previewFrame.setAttribute("src", "");
+
+    if (!fileInput.value) {
+        return;
+    }
+
+    //Get the image
+    var file = fileInput.files[0];
+    //Check file type 
+    if (file.type !== 'image/jpeg' && file.type !== 'image/png' && file.type !== 'image/gif' && file.type !== 'image/bmp') {
+        alert("Please upload image file");
+        return;
+    }
+
+    //Read and load image to preview
+    var reader = new FileReader();
+    reader.addEventListener('load', function (e) {
+        var data = e.target.result;
+        previewFrame.setAttribute('src', data);
+    });
+    reader.readAsDataURL(file);
+});
